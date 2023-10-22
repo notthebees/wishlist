bc362d3dbc7102022992ca29b06f79a271ea3601
paddy <paddy@paddy.com>
2023-10-22 11:11:44 +0100

    Add wish creation

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -6,4 +6,23 @@ class WishesController < ApplicationController
   def show
     @wish = Wish.find(params[:id])
   end
+
+  def new
+    @wish = Wish.new
+  end
+
+  def create
+    @wish = Wish.new(wish_params)
+
+    if @wish.save
+      redirect_to @wish
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def wish_params
+    params.require(:wish).permit(:title, :body)
+  end
 end
