70dda5a07f777eff2ba146f191794d8965e807a7
Patrick McGovern <patrick.mcgovern@forgerock.com>
2023-11-02 14:26:01 +0000

    Validate user email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: "test@example.com", password: "password")
+  end
+
+  test "is valid" do
+    assert @user.valid?
+  end
+
+  test "email is present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
 end
