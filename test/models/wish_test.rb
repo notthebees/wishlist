f3f6a1ba3b78e22016130b24222e153ca4d45a20
Patrick McGovern <patrick.mcgovern@forgerock.com>
2023-11-02 14:30:54 +0000

    Add tests for Wish

--- test/models/wish_test.rb
+++ test/models/wish_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class WishTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create!(email: "test@example.com", password: "password")
+    @wish = @user.wishes.build(title: "Test Wish", body: "This is a test wish.")
+  end
+
+  test "is valid" do
+    assert @wish.valid?
+  end
+
+  test "title is present" do
+    @wish.title = ""
+    assert_not @wish.valid?
+  end
 end
