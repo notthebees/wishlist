97df9b9d5ce95febdb2e50f9720bf923f8be8b20
Patrick McGovern <patrick.mcgovern@forgerock.com>
2023-11-02 14:48:23 +0000

    Add tests for wishes controller

--- test/controllers/wishes_controller_test.rb
+++ test/controllers/wishes_controller_test.rb
@@ -1,8 +1,27 @@
 require "test_helper"
 
 class WishesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get wishes_index_url
+  def setup
+    @user = User.create!(email: "test@example.com", password: "password")
+    @wish = @user.wishes.build(title: "Test Wish", body: "This is a test wish.")
+  end
+
+  test "redirects to login page if unauthenticated" do
+    get wishes_url
+    assert_redirected_to login_url
+    get wishes_url(@wish)
+    assert_redirected_to login_url
+  end
+
+  test "gets wishes if authenticated" do
+    post login_url, params: { session: { email: @user.email, password: 'password' } }
+    get wishes_url
+    assert_response :success
+  end
+
+  test "shows wish if authenticated" do
+    post login_url, params: { session: { email: @user.email, password: 'password' } }
+    get wishes_url(@wish)
     assert_response :success
   end
 end
