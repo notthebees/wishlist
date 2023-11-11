02143c59be41b9e16f37f0aea8078782fbb7aef6
paddy <paddy@paddy.com>
2023-11-11 14:16:32 +0000

    Add Friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :wishes
   validates :email, presence: true
+  has_many :friendships
+  has_many :friends, through: :friendships
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_02_120402) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_11_141308) do
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "friend_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -29,5 +38,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_02_120402) do
     t.index ["user_id"], name: "index_wishes_on_user_id"
   end
 
+  add_foreign_key "friendships", "friends"
+  add_foreign_key "friendships", "users"
   add_foreign_key "wishes", "users"
 end
